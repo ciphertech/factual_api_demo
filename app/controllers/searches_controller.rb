80796323d04620a43db2b37447017659486a8754
Pratik <pratik.ganvir@cipher-tech.com>
2014-02-14 18:52:29 +0530

    Added city and zipcode search functionality

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,14 +7,24 @@ class SearchesController < ApplicationController
   #--
   # To display search result
   #++
-  def search_by_choice
-    if params[:search_category].present? && params[:search_business].present?
+  def search_by_choice  
+    if params[:city].present?
       require 'factual'
       factual = Factual.new(KEY, SECRET)
-      @search_result= factual.table("places").filters("category_labels"=>{"$includes_any"=>[params[:search_category]]},"name"=> params[:search_business]).rows
-      flash[:notice]= ""
+      if params[:search_category].present? &&  params[:search_business].present?
+        @search_result= factual.table("places").filters("$and"=>[{"category_labels"=>{"$includes_any"=>[params[:search_category]]}},{"name"=>params[:search_business]}],"$or"=>[{"locality"=>params[:city]},{"postcode"=>params[:city]}]).rows
+        flash[:notice]= ""
+      elsif params[:search_category].present?
+        @search_result= factual.table("places").filters("category_labels"=>{"$includes_any"=>[params[:search_category]]},"$or"=>[{"locality"=>params[:city]},{"postcode"=>params[:city]}]).rows
+        flash[:notice]= ""
+      elsif params[:search_business].present? 
+        @search_result= factual.table("places").filters("name"=> params[:search_business],"$or"=>[{"locality"=>params[:city]},{"postcode"=>params[:city]}]).rows
+        flash[:notice]= ""
+      else
+        flash[:notice]= "Please enter Category name or Business name"
+      end
     else
-      flash[:notice]= "Please enter both Category name and Business name"
+      flash[:notice]= "Please enter city"
     end
   end
 
